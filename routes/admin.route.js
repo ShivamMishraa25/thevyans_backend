28536ee035734297501c15944ff556b7290d2c5c
shivammishraa25 <shivam.m4464@gmail.com>
2025-08-16 04:43:44 +0530

    admin panel is done... login is done... jwt,navigate on logout,edge cases done... just make delete post controller in backend

---
+++ routes/admin.route.js
@@ -0,0 +1,8 @@
+import { login, register } from "../controllers/admin.controller.js";
+
+function adminRoutes(app) {
+    app.post("/login", login);
+    app.post("/register", register);
+}
+
+export default adminRoutes;
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,10 @@
 import { createPost, getPosts } from "../controllers/post.controller.js";
 import upload from "../middleware/upload.js";
+import auth from "../middleware/auth.js";
 
 function postRoute(app) {
     app.get("/posts", getPosts);
-    app.post("/post", upload.array("images"), createPost);
+    app.post("/post", auth, upload.array("images"), createPost);
 }
 
 export default postRoute;
